8d9349a05c8c65df47c6da51cb78855988d07a46
Shashank <shashankdevulapally569@gmail.com>
2024-09-11 11:33:19 +0530

    deleted:    AI.html 	deleted:    login.html 	modified:   public/css/style.css 	modified:   server/routes/main.js 	deleted:    signup.html 	deleted:    viewblog.html 	new file:   views/AI.ejs 	new file:   views/login.ejs 	modified:   views/partials/header.ejs 	new file:   views/signup.ejs 	new file:   views/viewblog.ejs 	new file:   views/wcblog.ejs 	deleted:    wcblog.html

--- server/routes/main.js
+++ server/routes/main.js
@@ -15,4 +15,64 @@ router.get('', async (req, res) => {
         console.log(error);
     }
 });
+router.get('/wcblog', async (req, res) => {
+    try {
+        const locals = {
+            title: "Write Blog",
+            description: "Blog web"
+        }
+        
+        res.render('wcblog', {locals});
+    } catch (error) {
+        console.log(error);
+    }
+});
+router.get('/viewblog', async (req, res) => {
+    try {
+        const locals = {
+            title: "View Blogs",
+            description: "Blog web"
+        }
+        
+        res.render('viewblog', {locals});
+    } catch (error) {
+        console.log(error);
+    }
+});
+router.get('/AI', async (req, res) => {
+    try {
+        const locals = {
+            title: "AI",
+            description: "Blog web"
+        }
+        
+        res.render('AI', {locals});
+    } catch (error) {
+        console.log(error);
+    }
+});
+router.get('/login', async (req, res) => {
+    try {
+        const locals = {
+            title: "login",
+            description: "Blog web"
+        }
+        
+        res.render('login', {locals});
+    } catch (error) {
+        console.log(error);
+    }
+});
+router.get('/signup', async (req, res) => {
+    try {
+        const locals = {
+            title: "signup",
+            description: "Blog web"
+        }
+        
+        res.render('signup', {locals});
+    } catch (error) {
+        console.log(error);
+    }
+});
 module.exports = router;
