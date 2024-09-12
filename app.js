a4ed237924e75098c1f9e9a54248c368ef56ae2e
Shashank <shashankdevulapally569@gmail.com>
2024-09-12 11:53:16 +0530

    modified:   app.js 	new file:   server/config/db.js 	new file:   server/models/posts.js

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ require('dotenv').config();
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 // const cookieParser = require('cookie-parser');
-// const MongoStore = require('connect-mongo');
+const MongoStore = require('connect-mongo');
 
-// const connectDB = require('./server/config/db');
+const connectDB = require('./server/config/db');
 // const session = require('express-session');
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
 
 // Connect to database
-// connectDB();
+connectDB();
 
 // app.use(express.urlencoded({extended:true}));
 // app.use(express.json());
